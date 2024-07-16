940952300962a3f5d190146aa46f8f9f18217cb9
Łukasz <123584186+Wardiusz@users.noreply.github.com>
2024-07-16 16:15:55 +0200

    Initial Commit2 :D

--- src/main/java/com/wardiusz/test/Main.java
+++ src/main/java/com/wardiusz/test/Main.java
@@ -3,5 +3,6 @@ package com.wardiusz.test;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Goodbye world!");
     }
 }
